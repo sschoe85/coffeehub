fbbf21d202e5ef74927d43fd75fdb7c46347ab55
sschoe85 <s@bine-schoetensack.eu>
2020-07-22 13:38:19 +0200

    productList

---
+++ src/components/productList/productList.stories.js
@@ -0,0 +1,7 @@
+import "./productList.css";
+import productList from "./productList.html";
+
+export default { title: "productList" };
+
+export const coffeeList = () => productList;
+                        
